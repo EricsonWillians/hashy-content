089d5b66ab73c9617f23ca96cb3ce349fe0cdeda
ericsonwillians <rederickdeathwill@gmail.com>
2018-10-04 00:48:23 -0300

    Now it's possible to pass a function to be called when the content is loaded for a specific hash.

--- js/hashy-content.js
+++ js/hashy-content.js
@@ -1,11 +1,11 @@
 var hashes = {};
 
-$.fn.addHash = function (hash, contentURL) {
+$.fn.addHash = function (hash, contentURL, f) {
     var id = this.attr("id");
     if (typeof hashes[id] === 'undefined') {
         hashes[id] = [];
     }
-    hashes[id].push([hash, contentURL]);
+    hashes[id].push([hash, contentURL, f]);
 }
 
 $.fn.watch = function () {
@@ -14,27 +14,39 @@ $.fn.watch = function () {
         for (var hashAssociationIndex in hashes[insertionId]) {
             var _hash = hashes[insertionId][hashAssociationIndex][0];
             var contentURL = hashes[insertionId][hashAssociationIndex][1];
+            var f = hashes[insertionId][hashAssociationIndex][2];
             if (_hash === "index") {
                 if (location.hash === "") {
-                    window.location.hash = "#index";
+                   window.location.hash = "#index";
+                   location.reload();
                 }
             }
             if (location.hash.includes(_hash)) {
-                $('#' + insertionId).load(contentURL).hide().fadeIn();
+                var _f = f;
+                $('#' + insertionId).load(contentURL, function (data) {
+                    if (typeof _f != 'undefined') {
+                        _f();
+                    }
+                }).hide().fadeIn();
             }
         }
     }
+};
 
-    $(window).on('hashchange', function () {
-        for (var insertionId in hashes) {
-            for (var hashAssociationIndex in hashes[insertionId]) {
-                var _hash = hashes[insertionId][hashAssociationIndex][0];
-                var contentURL = hashes[insertionId][hashAssociationIndex][1];
-                if (location.hash.slice(1) === _hash) {
-                    $('#' + insertionId).load(contentURL).hide().fadeIn();
-                }
+$(window).on('hashchange', function () {
+    for (var insertionId in hashes) {
+        for (var hashAssociationIndex in hashes[insertionId]) {
+            var _hash = hashes[insertionId][hashAssociationIndex][0];
+            var contentURL = hashes[insertionId][hashAssociationIndex][1];
+            var f = hashes[insertionId][hashAssociationIndex][2];
+            if (location.hash.slice(1) === _hash) {
+                var _f = f;
+                $('#' + insertionId).load(contentURL, function (data) {
+                    if (typeof _f != 'undefined') {
+                        _f();
+                    }
+                }).hide().fadeIn();
             }
         }
-    });
-    
-};
+    }
+});
